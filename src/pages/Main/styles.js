45978861156aae13f5023e2c4cbf330d72907f62
Hugo <hugohoffmann04@gmail.com>
2019-09-16 16:58:18 -0300

    new: repositories list

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -67,3 +67,25 @@ export const SubmitButton = styled.button.attrs(props => ({
         }
     `}
 `;
+
+export const List = styled.ul`
+    list-style:none;
+    margin-top: 30px;
+
+    li{
+        padding: 15px 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items:center;
+    
+    & + li{
+        border-top: 1px solid #eee;
+    }
+
+    a{
+        color: #ff9033;
+        text-decoration: none;
+    }
+    }
+`;
