90bf860502d19f7200b8d8a28f397eaa584c6e5f
Hugo <hugohoffmann04@gmail.com>
2019-09-16 17:13:43 -0300

    new: route navegate to repository

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-export default function Repository() {
+export default function Repository({ match }) {
   return (
     <h1>Repository</h1>
   );
