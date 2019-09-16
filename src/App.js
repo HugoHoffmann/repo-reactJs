140e5e4bdce2ee53d1d25e1a8e2b95aad24626d5
Hugo <hugohoffmann04@gmail.com>
2019-09-16 14:34:57 -0300

    new: global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Routes from './routes';
-
+import GlobalStyle from './styles/global';
 function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
   );
 }
 
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Title } from './styles';
 
 export default function Main() {
   return (
-    <h1> Main</h1> 
+    <Title> Main</Title> 
   );
 }
