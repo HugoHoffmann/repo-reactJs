ab51d77961441b7b6579a505cfa028e6b860d744
Hugo <hugohoffmann04@gmail.com>
2019-09-17 08:27:34 -0300

    chore: define prop-types

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from  'prop-types';
 import api from '../../services/api';
 // import { Container } from './styles';
 
 export default class Repository extends Component{
+  static PropTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      })
+    }).isRequired
+  };
   state = {
     repository: {},
     issues: [],
